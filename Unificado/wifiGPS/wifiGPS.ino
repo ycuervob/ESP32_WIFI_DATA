9d9ef72d0393de7918414e9e7713c928a7756d3f
ycuervob <ycuervob@unal.edu.co>
2022-09-09 15:31:33 -0500

    print serial gps

--- Unificado/wifiGPS/wifiGPS.ino
+++ Unificado/wifiGPS/wifiGPS.ino
@@ -72,6 +72,16 @@ void loop() {
   float flat, flon;
   unsigned long age;
   gps.f_get_position(&flat, &flon, &age);
+
+  gps.f_get_position(&flat, &flon, &age);
+  Serial.print("LAT=");
+  Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+  Serial.print(" LON=");
+  Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+  Serial.print(" SAT=");
+  Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+  Serial.print(" PREC=");
+  Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0.0 : ((float) gps.hdop())/100 , 6);
   
   //Check WiFi connection status
   if(WiFi.status()== WL_CONNECTED){
