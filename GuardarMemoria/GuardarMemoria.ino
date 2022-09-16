fd42fbc5a252acc50389af27815bcb867928732d
ycuervob <ycuervob@unal.edu.co>
2022-09-16 14:55:02 -0500

    ejemplo simple de guardar en archivos

--- GuardarMemoria/GuardarMemoria.ino
+++ GuardarMemoria/GuardarMemoria.ino
@@ -1,41 +1,64 @@
+/*
+  SD card read/write
+
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+*/
+
+#include <SPI.h>
 #include <SD.h>
- 
+
 File myFile;
- 
-void setup()
-{
+
+void setup() {
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
   Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
-   pinMode(10, OUTPUT);
- 
-  if (!SD.begin(10)) {
+
+  if (!SD.begin(SS)) {
     Serial.println("initialization failed!");
-    return;
+    while (1);
   }
   Serial.println("initialization done.");
- 
-  // open the file for reading:
-  myFile = SD.open("test.txt");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("/data.json",FILE_APPEND);
+
+  // if the file opened okay, write to it:
   if (myFile) {
-    Serial.println("test.txt:");
- 
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-    	Serial.write(myFile.read());
+    Serial.print("Writing to data.json...");
+   
+    for(int i = 0; i<100; i++){
+      myFile.println(i);
     }
     // close the file:
     myFile.close();
+    Serial.println("done.");
   } else {
-  	// if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    // if the file didn't open, print an error:
+    Serial.println("error opening data.json");
   }
 }
- 
-void loop()
-{
-	// nothing happens after setup
+
+void loop() {
+  // nothing happens after setup
 }
