6d988eabd361467c16d2be64f87ec3a3eb564474
ycuervob <64037012+ycuervob@users.noreply.github.com>
2023-02-11 03:24:00 -0500

    Refactorización del código, añade .h por cada archivo y organiza el proyecto en en carperta src no se hizo cambios funcionales

--- Unificado/Unificado.ino
+++ Unificado/Unificado.ino
@@ -1,5 +1,12 @@
 #include <Arduino.h>
-#include "utilities.h"
+#include "src/control/wrappers.h"
+#include "src/func/generalUtilities.h"
+#include "src/componentes/pinnesUtilities.h"
+#include "src/componentes/acelerometroUtilities.h"
+#include "src/componentes/gpsUtilities.h"
+#include "src/componentes/tempUtilities.h"
+#include "src/componentes/acelerometroUtilities.h"
+#include "src/componentes/termocuplaUtilities.h"
 
 /**
  * @brief Funcion de inicio del programa.
