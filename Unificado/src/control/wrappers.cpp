43ab6eb9386ae0b50ae757bdf1f186c0f24458ee
Yeison Cuervo Basurto <ycuervob@unal.edu.co>
2023-02-23 02:52:49 -0500

    Especificación de modo de espera
    Se encienden todos los leds en modo de espera

--- Unificado/src/control/wrappers.cpp
+++ Unificado/src/control/wrappers.cpp
@@ -1,26 +1,32 @@
 #include <Arduino.h>
+#include "../componentes/pinnesUtilities.h"
 
 /**
  * @brief Wrapper que ejecuta una funcion por un tiempo determinado.
- * 
- * @param time 
- * @param func 
+ *
+ * @param time
+ * @param func
  */
-void timeWrapper(unsigned long time, void (*func)()) {
-  for (unsigned long start = millis(); millis() - start < time;) {
+void timeWrapper(unsigned long time, void (*func)())
+{
+  for (unsigned long start = millis(); millis() - start < time;)
+  {
     func();
   }
 }
 
 /**
  * @brief Wrapper que ejecuta una funcion por un tiempo determinado.
- * 
- * @param time 
+ *
+ * @param time
  * @param func byte 2: NO MAS DATOS
  */
-void timeWrapper(unsigned long time, byte (*func)()) {
-  for (unsigned long start = millis(); millis() - start < time;) {
-    if(func() == 2){
+void timeWrapper(unsigned long time, byte (*func)())
+{
+  for (unsigned long start = millis(); millis() - start < time;)
+  {
+    if (func() == 2)
+    {
       return;
     }
   }
@@ -28,16 +34,27 @@ void timeWrapper(unsigned long time, byte (*func)()) {
 
 /**
  * @brief Wrapper que ejecuta una funcion por un tiempo determinado, se usa para ejecutar la funcion de reposo, este se ejecuta hasta que pase el tiempo o hasta que se registre una aceleración anormal.
- * 
- * @param time 
+ *
+ * @param time
  * @param min_acelerometro | minimo de aceleracion para que se ejecute la funcion
- * @param func 
+ * @param func
  */
-void timeWrapper(unsigned long time, int min_acelerometro, bool (*func)(int)) {
-  for (unsigned long start = millis(); millis() - start < time;) {
+void timeWrapper(unsigned long time, int min_acelerometro, bool (*func)(int))
+{
+  encenderLed('c', true);
+  encenderLed('e', true);
+  encenderLed('w', true);
+  encenderLed('s', true);
+  for (unsigned long start = millis(); millis() - start < time;)
+  {
     bool mybool = func(min_acelerometro);
-    if (mybool) {
+    if (mybool)
+    {
+      encenderLed('c', false);
+      encenderLed('e', false);
+      encenderLed('w', false);
+      encenderLed('s', false);
       return;
-    } 
+    }
   }
 }
